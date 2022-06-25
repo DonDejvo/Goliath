31ebfe98ba03f5e6320d4fc6a2a27574963118c0
DonDejvo <dada542cz@gmail.com>
2022-06-25 20:58:38 +0200

    Documentation added

--- src/game.js
+++ src/game.js
@@ -1,5 +1,12 @@
 import { Screen } from "./screen.js";
 
+/**
+ * <p>An instance of <code>Game</code> must be passed into initialization.</p>
+ * <p>All methods are called after WebGL context exists. So you can safely create and manipulate graphics resources.</p>
+ * <p>This allows easily to have multiple screens.</p>
+ * 
+ * @author DonDejvo
+ */
 class Game {
 
     /**
@@ -10,22 +17,45 @@ class Game {
 
     constructor() {}
 
+    /**
+     * 
+     */
     preload() {}
 
+    /**
+     * Called right after initialization.
+     */
     create() {}
 
+    /**
+     * Called when window is resized.
+     * 
+     * @param {number} width the new width in pixels
+     * @param {number} height the new height in pixels
+     */
     resize(width, height) {
         if(this.screen) {
             this.screen.resize(width, height);
         }
     }
 
+    /**
+     * Called when the game should render itself.
+     * 
+     * @param {number} delta the time in seconds since the last render
+     */
     render(delta) {
         if(this.screen) {
             this.screen.render(delta);
         }
     }
 
+    /**
+     * Sets the current screen. {@link Screen#hide} is called on any old 
+     * screen and {@link Screen#show} is called on the new screen.
+     * 
+     * @param {Screen} screen may be {@code null}
+     */
     setScreen(screen) {
         if(this.screen) {
             this.screen.hide();
--- src/screen.js
+++ src/screen.js
@@ -1,4 +1,10 @@
-
+/**
+ * Represents one of game screens, such as a main menu, a settings menu, the game screen and so on.
+ * 
+ * @author DonDejvo
+ * 
+ * @see Game
+ */
 class Screen {
 
     constructor() {
@@ -7,12 +13,26 @@ class Screen {
 
     create() {}
 
+    /**
+     * Called when this screen becomes the current screen for a {@link Game}.
+     */
     show() {}
 
+    /**
+     * Called when this screen is no longer current scene for a {@link Game}.
+     */
     hide() {}
 
+    /**
+     * @see Game#resize
+     */
     resize(width, height) {}
 
+    /**
+     * Called when the screen should render itself.
+     * 
+     * @param {number} delta the time in seconds since the last render
+     */
     render(delta) {}
 
     dispose() {}
