27599b213d787f5490d5ffe4f69997c6835e3b29
Revma <revmapublic@protonmail.com>
2022-07-07 23:10:02 +0200

    generated docs

--- docs/scripts/linenumber.js
+++ docs/scripts/linenumber.js
@@ -1,7 +1,6 @@
 /*global document */
-( () => {
-
-    const source = document.getElementsByClassName( 'prettyprint source linenums' );
+(() => {
+    const source = document.getElementsByClassName('prettyprint source linenums');
     let i = 0;
     let lineNumber = 0;
     let lineId;
@@ -9,26 +8,18 @@
     let totalLines;
     let anchorHash;
 
-    if ( source && source[ 0 ] ) {
-
-        anchorHash = document.location.hash.substring( 1 );
-        lines = source[ 0 ].getElementsByTagName( 'li' );
+    if (source && source[0]) {
+        anchorHash = document.location.hash.substring(1);
+        lines = source[0].getElementsByTagName('li');
         totalLines = lines.length;
 
-        for ( ; i < totalLines; i++ ) {
-
+        for (; i < totalLines; i++) {
             lineNumber++;
             lineId = `line${lineNumber}`;
-            lines[ i ].id = lineId;
-
-            if ( lineId === anchorHash ) {
-
-                lines[ i ].className += ' selected';
-
+            lines[i].id = lineId;
+            if (lineId === anchorHash) {
+                lines[i].className += ' selected';
             }
-
         }
-
     }
-
-} )();
+})();
